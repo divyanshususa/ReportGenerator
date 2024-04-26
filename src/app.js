aef3a0001de458142425c69ebfba9960f5b490b8
nowcare <prakhar@nowcare4u.com>
2024-04-26 09:31:29 +0530

    custom header

--- src/app.js
+++ src/app.js
@@ -18,8 +18,15 @@ app.use(helmet());
 
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use(cors({
-    origin: ["*"],
+    origin: "*",
     credentials: true
 }));
 
